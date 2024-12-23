2d70e9f2bb9d7023a12ab922044d574d5d6bf725
dusehd1 <101052631+0yeonnnn0@users.noreply.github.com>
2024-12-23 04:13:51 -0800

    [Gold III] Title: 앱, Time: 292 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.py
+++ 백준/Gold/7579. 앱/앱.py
@@ -6,10 +6,8 @@ max_cost = sum(cost)
 dp = [0] * (max_cost + 1)
 
 for i in range(N):
-    for j in range(max_cost, cost[i] - 1, -1):
-        dp[j] = max(dp[j], dp[j - cost[i]] + memory[i])
+    for c in range(max_cost, cost[i] - 1, -1):
+        dp[c] = max(dp[c], dp[c - cost[i]] + memory[i])
 
-for i in range(max_cost + 1):
-    if dp[i] >= M:
-        print(i)
-        break
+result = min(c for c in range(max_cost + 1) if dp[c] >= M)
+print(result)
