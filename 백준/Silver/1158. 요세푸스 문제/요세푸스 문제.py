597ce90e0ad346c698ac1adc70c8f8474604f32c
dusehd1 <101052631+0yeonnnn0@users.noreply.github.com>
2024-12-23 04:08:21 -0800

    [Silver IV] Title: 요세푸스 문제, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,12 +1,10 @@
-from collections import deque
-
 N, K = map(int, input().split())
-queue = deque(range(1, N + 1))
-josephus_sequence = []
+people = list(range(1, N + 1))
+result = []
+index = 0
 
-while queue:
-    for _ in range(K - 1):
-        queue.append(queue.popleft())  # K-1번 앞의 사람을 뒤로 보냅니다
-    josephus_sequence.append(queue.popleft())  # K번째 사람을 제거합니다
+while people:
+    index = (index + K - 1) % len(people)  # K번째 사람의 인덱스 계산
+    result.append(people.pop(index))  # K번째 사람 제거 후 결과에 추가
 
-print('<' + ', '.join(map(str, josephus_sequence)) + '>')
+print('<' + ', '.join(map(str, result)) + '>')  # 결과 출력
