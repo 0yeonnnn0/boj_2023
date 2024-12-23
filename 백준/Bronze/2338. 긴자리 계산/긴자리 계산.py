1a37019974ed297ae3ab8ed9c880d722c32a2c09
dusehd1 <101052631+0yeonnnn0@users.noreply.github.com>
2024-12-23 03:59:18 -0800

    [Bronze V] Title: 긴자리 계산, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2338. 긴자리 계산/긴자리 계산.py
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.py
@@ -1,10 +1,14 @@
-A = int(input())
-B = int(input())
+A = int(input())  # 첫 번째 수를 입력받습니다.
+B = int(input())  # 두 번째 수를 입력받습니다.
 
-sum = A + B
-difference = A - B
-product = A * B
+# A와 B의 합을 계산합니다.
+sum_result = A + B
+# A와 B의 차를 계산합니다.
+difference_result = A - B
+# A와 B의 곱을 계산합니다.
+product_result = A * B
 
-print(sum)
-print(difference)
-print(product)
+# 각 결과를 출력합니다.
+print(sum_result)
+print(difference_result)
+print(product_result)
