83783f3925e76fa094e131a3b55575418493f36c
dusehd1 <101052631+0yeonnnn0@users.noreply.github.com>
2024-12-23 04:11:47 -0800

    [Silver IV] Title: 보물, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.py
+++ 백준/Silver/1026. 보물/보물.py
@@ -2,11 +2,13 @@ N = int(input())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-A.sort()  # A를 오름차순 정렬
-B.sort(reverse=True)  # B를 내림차순 정렬
+# A를 오름차순 정렬
+A.sort()
+# B를 내림차순 정렬
+B.sort(reverse=True)
 
 S = 0
 for i in range(N):
-    S += A[i] * B[i]  # A와 B의 각 요소의 곱을 S에 더함
+    S += A[i] * B[i]
 
-print(S)  # 최소 S 값 출력
+print(S)
