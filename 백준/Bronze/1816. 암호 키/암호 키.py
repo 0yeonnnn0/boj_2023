44f18da73e8c5527d5e1b04d00afe01ed63acfa9
dusehd1 <101052631+0yeonnnn0@users.noreply.github.com>
2024-12-23 03:51:40 -0800

    [Bronze I] Title: 암호 키, Time: 236 ms, Memory: 43916 KB -BaekjoonHub

--- 백준/Bronze/1816. 암호 키/암호 키.py
+++ 백준/Bronze/1816. 암호 키/암호 키.py
@@ -1,21 +1,24 @@
-MAX_PRIME = 1000000
-is_prime = [True] * (MAX_PRIME + 1)
-is_prime[0] = is_prime[1] = False
+def sieve_of_eratosthenes(limit):
+    is_prime = [True] * (limit + 1)
+    p = 2
+    while (p * p <= limit):
+        if (is_prime[p] == True):
+            for i in range(p * p, limit + 1, p):
+                is_prime[i] = False
+        p += 1
+    primes = []
+    for p in range(2, limit + 1):
+        if is_prime[p]:
+            primes.append(p)
+    return primes
 
-for i in range(2, int(MAX_PRIME**0.5) + 1):
-    if is_prime[i]:
-        for j in range(i * i, MAX_PRIME + 1, i):
-            is_prime[j] = False
-
-primes = [i for i in range(2, MAX_PRIME + 1) if is_prime[i]]
+primes = sieve_of_eratosthenes(1000000)
 
 N = int(input())
 for _ in range(N):
     S = int(input())
     is_valid_key = True
     for prime in primes:
-        if prime * prime > S:
-            break
         if S % prime == 0:
             is_valid_key = False
             break
