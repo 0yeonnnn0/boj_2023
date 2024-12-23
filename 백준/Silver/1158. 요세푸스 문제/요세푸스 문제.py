d1e5efea9211661fb9f99a394a40b45b32b81617
dusehd1 <101052631+0yeonnnn0@users.noreply.github.com>
2024-12-23 04:06:41 -0800

    [Silver IV] Title: 요세푸스 문제, Time: 2328 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -6,7 +6,7 @@ result = []
 
 while queue:
     for _ in range(K - 1):
-        queue.append(queue.popleft())  # K-1번째 사람을 뒤로 보냄
-    result.append(queue.popleft())  # K번째 사람을 제거하고 결과에 추가
+        queue.append(queue.popleft())  # K-1번 만큼 앞에서 꺼내 뒤로 삽입
+    result.append(queue.popleft())  # K번째 사람 제거
 
 print('<' + ', '.join(map(str, result)) + '>')
