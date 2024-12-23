2992aa90427ff216db545928e6f1c3532c3935dd
dusehd1 <101052631+0yeonnnn0@users.noreply.github.com>
2024-12-23 03:57:24 -0800

    [Bronze I] Title: 팀 이름 정하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1296. 팀 이름 정하기/팀 이름 정하기.py
+++ 백준/Bronze/1296. 팀 이름 정하기/팀 이름 정하기.py
@@ -1,23 +1,18 @@
-yeondoo_name = input().strip()
-N = int(input().strip())
-best_team = ""
+yeondoo_name = input()
+N = int(input())
 max_probability = -1
+best_team_name = ''
 
 for _ in range(N):
     team_name = input().strip()
-    count_L = count_O = count_V = count_E = 0
-    for char in yeondoo_name + team_name:
-        if char == 'L':
-            count_L += 1
-        elif char == 'O':
-            count_O += 1
-        elif char == 'V':
-            count_V += 1
-        elif char == 'E':
-            count_E += 1
-    probability = ((count_L + count_O) * (count_L + count_V) * (count_L + count_E) * (count_O + count_V) * (count_O + count_E) * (count_V + count_E)) % 100
-    if probability > max_probability or (probability == max_probability and team_name < best_team):
+    combined_name = yeondoo_name + team_name
+    L = combined_name.count('L')
+    O = combined_name.count('O')
+    V = combined_name.count('V')
+    E = combined_name.count('E')
+    probability = ((L + O) * (L + V) * (L + E) * (O + V) * (O + E) * (V + E)) % 100
+    if probability > max_probability or (probability == max_probability and team_name < best_team_name):
         max_probability = probability
-        best_team = team_name
+        best_team_name = team_name
 
-print(best_team)
+print(best_team_name)
