713535304ba7babfe2d4ae628c6607e13a55487e
dusehd1 <101052631+0yeonnnn0@users.noreply.github.com>
2026-01-22 17:02:45 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -0,0 +1,7 @@
+function solution(num_list) {
+    var answer = [];
+    
+    num_list.at(-1) > num_list.at(-2) ? num_list.push(num_list.at(-1) - num_list.at(-2)) : num_list.push(num_list.at(-1) * 2)
+    
+    return num_list;
+}
