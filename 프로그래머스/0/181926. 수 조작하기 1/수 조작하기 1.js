88bac8257692dc71b211994b8a0fe902981fac75
dusehd1 <101052631+0yeonnnn0@users.noreply.github.com>
2026-01-22 17:09:27 +0900

    [level 0] Title: 수 조작하기 1, Time: 3.68 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -2,14 +2,11 @@ function solution(n, control) {
     var answer = 0;
     
     for (let i = 0; i < control.length; i++){
-        if (control[i] == "w"){
-            n += 1
-        } else if (control[i] == "s"){
-            n -= 1
-        } else if (control[i] == "d"){
-            n += 10
-        } else {
-            n -= 10
+        switch(control[i]){
+            case "w" : n += 1; break
+            case "s" : n -= 1; break
+            case 'd' : n += 10; break
+            case 'a' : n -= 10; break
         }
     }
     
