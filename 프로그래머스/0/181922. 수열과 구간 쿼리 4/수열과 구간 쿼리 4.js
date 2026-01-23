ee89c564881aa2beca978a566b19ed8242e4dfce
dusehd1 <101052631+0yeonnnn0@users.noreply.github.com>
2026-01-23 15:53:50 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 5.96 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -0,0 +1,20 @@
+function solution(arr, queries) {
+    var answer = [];
+    
+    for (let i = 0; i < queries.length; i++){
+        let s = queries[i][0]
+        let e = queries[i][1]
+        let k = queries[i][2]
+        
+        for (let j = s; j <= e; j++){
+            if (j % k === 0){
+                arr[j] += 1
+            }
+        }
+        
+    }
+    
+    answer = arr
+    
+    return answer;
+}
