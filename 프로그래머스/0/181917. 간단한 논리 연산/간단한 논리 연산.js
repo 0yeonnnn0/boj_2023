81371a653bee319ca7d84c92fd962eb42822c948
dusehd1 <101052631+0yeonnnn0@users.noreply.github.com>
2026-01-27 15:41:42 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.js
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.js
@@ -1,15 +1,3 @@
-function solution(x1, x2, x3, x4) {
-    var answer = true;
-    
-    function OR(a, b){
-        if (a || b){
-            return true
-        }
-    }
-    
-    
-    
-    
-    
+function solution(x1, x2, x3, x4) { 
     return (x1 || x2) && (x3 || x4);
 }
