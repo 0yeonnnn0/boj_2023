73b298940aa09ba249719909af513590f513a6cb
dusehd1 <101052631+0yeonnnn0@users.noreply.github.com>
2026-01-27 17:01:57 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 21.42 ms, Memory: 35 MB -BaekjoonHub

---
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -0,0 +1,11 @@
+function solution(my_string, queries) {
+    var answer = '';
+    let arr = my_string.split("")
+    
+    for (let [s, e] of queries){
+        let reversed = arr.slice(s, e + 1).reverse();
+        arr.splice(s, e - s + 1, ...reversed);
+
+    }
+    return arr.join("");
+}
